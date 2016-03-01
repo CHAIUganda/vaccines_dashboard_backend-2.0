12c0cc3243cd9da81affd1d26e5cad149b95d451
Malisa Ncube <malisa.ncube@gmail.com>
2016-03-01 10:37:15 +0300

    Minor changes to stock movement

--- src/app/stock/stock.js
+++ src/app/stock/stock.js
@@ -1,7 +1,8 @@
 angular.module( 'ngDashboard.stock', [
   'ui.router',
   'placeholders',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'http'
 ])
 
 .config(function config( $stateProvider ) {
@@ -17,13 +18,37 @@ angular.module( 'ngDashboard.stock', [
   });
 })
 
-.controller( 'StockCtrl', function StockCtrl( $scope ) {
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+.controller( 'StockCtrl', function StockCtrl( $scope, $http) {
+
+    var districts_json={};
+    var vaccines_json={};   
+
+    $scope.districts = [
+            {id: 1, name: "Kampala"},
+            {id: 2, name: "Jinja"},
+            {id: 3, name: "Abim"},
+    ];
+    
+    $scope.vaccines = [
+            {id: 1, name: "DPT3-HEB-HID"},
+            {id: 2, name: "DPT4-HEB-Other"},
+    ];
+    
+    $scope.filter=function(mode){
+        switch(mode){
+            case "district":
+                $scope.districts[$scope.district] = $scope.districts[$scope.district];
+                $scope.district = 'all';
+            break;
+            case "district":
+                $scope.vaccines[$scope.vaccine] = $scope.vaccines[$scope.vaccine];
+                $scope.vaccine = 'all';
+            break;
+        }
+        delete $scope.districts["all"];
+        delete $scope.vaccines["all"];
+    }
+  
 })
 
 ;
