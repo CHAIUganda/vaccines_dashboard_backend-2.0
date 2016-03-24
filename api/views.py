12718b7a8b717c30171a1457cc77185309fbbd0c
Malisa Ncube <malisa.ncube@gmail.com>
2016-03-24 14:55:04 +0300

    SEttings changes

--- api/views.py
+++ api/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Sum
 
 # Create your views here.
 from api.serializers import DistrictSerializer
-from home.models import District
+from home.models import District, DistrictBalance
 
 
 @api_view(['GET'])
-def district_list(request):
-    if request.method == 'GET':
-        districts = District.objects.all()
-        serializer = DistrictSerializer(districts)
-        return Response(serializer.data)
-
-
+def district_list(request, year, month_start, vaccine):
+    # if request.method == 'GET':
+    #     units = DistrictBalance.objects.filter(year=year, month=month_start, vaccine=vaccine
+    #                                   ).values('id', 'district_name'
+    #                                            ).annotate(issued=Sum('dose'))
+    #     serializer = DistrictSerializer(units)
+    #
+    #     return Response(serializer.data)
+    pass
--- dashboard/settings/prod.py
+++ dashboard/settings/prod.py
@@ -9,11 +9,11 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite://db.sqlite3')
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    #'default': dj_database_url.config(default='sqlite://db.sqlite3')
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'dashboard.db3'),
+     }
 }
 
 
