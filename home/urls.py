1b31d0f2139f1aedf86e176f22850d237db2b298
Malisa Ncube <malisa.ncube@gmail.com>
2016-04-11 10:55:40 +0300

    Reinitialise repo

--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, patterns
-from home.views import HomeView, ImportView
-
-urlpatterns = [
-    url('^$', ImportView, name='import_view'),
-]
-
--- run.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd..
-_mainenv\Scripts\activate
-cd dashboard
-python manage.py migrate
