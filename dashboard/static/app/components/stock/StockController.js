5c7bfe8c46f4a6aabd29dd7c4c05793a5c76252d
Malisa Ncube <malisa.ncube@gmail.com>
2016-04-27 11:32:45 +0300

    Added Nvd3

--- dashboard/static/app/components/stock/StockController.js
+++ dashboard/static/app/components/stock/StockController.js
@@ -1,9 +1,7 @@
 angular.module('dashboard')
-    .controller('StockController', ['$scope', 'ReportService', '$rootScope',
+    .controller('StockController', ['$scope', 'ReportService', '$rootScope', 'nvd3',
     function($scope, ReportService, $rootScope)
     {
-
-
 
         ReportService.getMonths().then(function(data) {
             $scope.months = data;
@@ -24,6 +22,50 @@ angular.module('dashboard')
         $scope.startMonth = "";
         $scope.endMonth = "";
         $scope.vaccine = "";
+
+        $scope.options = {
+            chart: {
+                type: 'discreteBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 55
+                },
+                x: function(d){ return d.label; },
+                y: function(d){ return d.value; },
+                showValues: true,
+                valueFormat: function(d){
+                    return d3.format(',.4f')(d);
+                },
+                transitionDuration: 500,
+                xAxis: {
+                    axisLabel: 'X Axis'
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: 30
+                }
+            }
+        };
+
+        $scope.data = [{
+            key: "Cumulative Return",
+            values: [
+                { "label" : "A" , "value" : -29.765957771107 },
+                { "label" : "B" , "value" : 0 },
+                { "label" : "C" , "value" : 32.807804682612 },
+                { "label" : "D" , "value" : 196.45946739256 },
+                { "label" : "E" , "value" : 0.19434030906893 },
+                { "label" : "F" , "value" : -98.079782601442 },
+                { "label" : "G" , "value" : -13.925743130903 },
+                { "label" : "H" , "value" : -5.1387322875705 }
+            ]
+        }];
+
+
+
 
     }
 
