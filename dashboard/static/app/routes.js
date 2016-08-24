d61ad2d86318109ce195586dac5bf550b4bdfc9e
smagero <magerosteve@gmail.com>
2016-08-24 12:11:18 +0300

    added uptake rate

--- dashboard/static/app/routes.js
+++ dashboard/static/app/routes.js
@@ -42,17 +42,17 @@ dashboard.config(["$stateProvider", "$urlRouterProvider", function($stateProvide
             .state("stock.consumption", {
                 url: "/consumption",
                 templateUrl: "static/app/components/stock/consumption.html",
-                controller: "ConsumptionController"
+                controller: "StockController"
             })
             .state("stock.monthsleft", {
                 url: "/monthsleft",
                 templateUrl: "static/app/components/stock/monthsstockleft.html",
-                controller: "MonthsStockLeftController"
+                controller: "StockController"
             })
             .state("stock.uptakerate", {
                 url: "/uptakerate",
                 templateUrl: "static/app/components/stock/uptakerate.html",
-                controller: "UptakeRateController"
+                controller: "StockController"
             })
             .state("stock.wastagerate", {
                 url: "/wastagerate",
