99901e69986e59977cffdeeae8a857fecf1b577e
smagero <magerosteve@gmail.com>
2016-08-29 11:18:19 +0300

    added graphs for distribution and months of stock left

--- dashboard/static/app/components/stock/stockController.js
+++ dashboard/static/app/components/stock/stockController.js
@@ -39,11 +39,11 @@ angular.module('dashboard')
                 // calculate totals
                 var total = 0;
                 for(var i = 0; i < vm.data.length; i++){
-                    var units = vm.data[i].at_hand;
-                    total += units;
+                    if (vm.data[i].at_hand == 0)
+                        total++;
                 }
 
-                shellScope.child.stockathand = total;
+                shellScope.child.stockedout = (total / vm.data.length) * 100;
 
                 // construct graph data
                 var graphdata = [];
@@ -92,7 +92,7 @@ angular.module('dashboard')
                                 left: 65
                             },
                             groupSpacing: 0.2,
-                            rotateLabels: 90,
+                            rotateLabels: 45,
                             staggerLabels: true,
                             x: function(d){ return d[0]; },
                             y: function(d){ return d[1]; },
@@ -119,6 +119,129 @@ angular.module('dashboard')
 
 ]);
 
+angular.module('dashboard')
+    .controller('MonthsStockLeftController', ['$scope', 'StockService', '$rootScope', 'NgTableParams', 'FilterService', 'MonthService',
+    function($scope, StockService, $rootScope, NgTableParams, FilterService, MonthService)
+    {
+        var vm = this;
+        var shellScope = $scope.$parent;
+        shellScope.child = $scope;
+
+        // Todo: Use this to sort by performance (Malisa)
+        vm.SortByKey = function(array, key) {
+            return array.sort(function(a, b) {
+                var x = a[key]; var y = b[key];
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            });
+        };
+
+        vm.getStockByDistrict = function(startMonth, endMonth, district, vaccine) {
+
+            vm.startMonth ? vm.startMonth : "Nov 2015";
+            vm.endMonth = vm.endMonth ? vm.endMonth : "Dec 2016";
+            //Todo: Temporarily disable filtering by district for the table
+            district = ""
+            vm.district = "";
+            vm.vaccine = vm.selectedVaccine ? vm.selectedVaccine.name : "";
+
+
+            StockService.getStockByDistrict(startMonth, endMonth, district, vaccine)
+                .then(function(data) {
+
+                vm.data = angular.copy(data);
+                vm.tableParams = new NgTableParams({
+                    page: 1,
+                    count: 15
+                }, {
+                    filterDelay: 0,
+                    counts: [],
+                    data: vm.data,
+                });
+
+                // calculate totals
+                var total = 0;
+                for(var i = 0; i < vm.data.length; i++){
+                    if ((vm.data[i].at_hand/vm.data[i].stock_requirement__maximum) < 0.5)
+                        total++;
+                }
+
+                shellScope.child.belowminimum = (total / vm.data.length) * 100;
+
+                // construct graph data
+                var graphdata = [];
+                var series = [];
+                var min_series = [];
+                var max_series = [];
+
+                for (var i = 0; i < vm.data.length ; i++) {
+                    series.push([vm.data[i].district_name, vm.data[i].at_hand])
+                    min_series.push([vm.data[i].district_name, vm.data[i].stock_requirement__minimum])
+                    max_series.push([vm.data[i].district_name, vm.data[i].stock_requirement__maximum])
+                }
+                graphdata.push({
+                        key: "Min",
+                        values: min_series
+                });
+                graphdata.push({
+                        key: "Stock At Hand",
+                        values: series
+                });
+
+                graphdata.push({
+                        key: "Max",
+                        values: max_series
+                });
+
+                vm.graph = graphdata;
+
+                // update graph
+                vm.options = {
+                        chart: {
+                            type: 'multiBarChart',
+                            height: 500,
+
+                            title: {
+                                enable: true,
+                                text: 'VACCINE STOCK LEVELS'
+                            },
+                            showLegend: true,
+                            stacked: true,
+                            showControls: true,
+                            margin : {
+                                top: 20,
+                                right: 20,
+                                bottom: 85,
+                                left: 65
+                            },
+                            groupSpacing: 0.2,
+                            rotateLabels: 45,
+                            staggerLabels: true,
+                            x: function(d){ return d[0]; },
+                            y: function(d){ return d[1]; },
+                            showValues: true,
+                            valueFormat: function(d){
+                                return tickFormat(d3.format(',.1f'));
+                            },
+                            transitionDuration: 500,
+                        }
+                };
+            });
+        };
+
+
+
+        $scope.$on('refresh', function(e, startMonth, endMonth, district, vaccine) {
+            if(startMonth.name && endMonth.name && district.name && vaccine.name)
+            {
+
+                vm.getStockByDistrict(startMonth.name, endMonth.name, district.name, vaccine.name);
+                vm.getStockByMonth(startMonth.name, endMonth.name, district.name, vaccine.name);
+            }
+        });
+
+    }
+
+]);
 
 angular.module('dashboard')
     .controller('ConsumptionController', ['$scope', 'StockService', '$rootScope', 'NgTableParams', 'FilterService', 'MonthService',
@@ -250,93 +373,122 @@ angular.module('dashboard')
 ]);
 
 angular.module('dashboard')
-    .controller('WastageRateController', ['$scope', 'StockService', '$rootScope',
-    function($scope, StockService, $rootScope)
+    .controller('DistributionController', ['$scope', 'StockService', '$rootScope', 'NgTableParams', 'FilterService', 'MonthService',
+    function($scope, StockService, $rootScope, NgTableParams, FilterService, MonthService)
     {
-    }
-
-
-]);
-
-angular.module('dashboard')
-    .controller('steve', function($scope){
         var vm = this;
         var shellScope = $scope.$parent;
         shellScope.child = $scope;
 
-        vm.options = {
-            chart: {
-                type: 'linePlusBarChart',
-                height: 500,
-                margin: {
-                    top: 30,
-                    right: 75,
-                    bottom: 50,
-                    left: 75
-                },
-                bars: {
-                    forceY: [0]
-                },
-                bars2: {
-                    forceY: [0]
-                },
-                color: ['#2ca02c', 'darkred'],
-                x: function(d,i) { return i },
-                xAxis: {
-                    axisLabel: 'X Axis',
-                    tickFormat: function(d) {
-                        var dx = vm.data[0].values[d] && vm.data[0].values[d].x || 0;
-                        if (dx > 0) {
-                            return d3.time.format('%x')(new Date(dx))
-                        }
-                        return null;
-                    }
-                },
-                x2Axis: {
-                    tickFormat: function(d) {
-                        var dx = vm.data[0].values[d] && vm.data[0].values[d].x || 0;
-                        return d3.time.format('%b-%Y')(new Date(dx))
-                    },
-                    showMaxMin: false
-                },
-                y1Axis: {
-                    axisLabel: 'Y1 Axis',
-                    tickFormat: function(d){
-                        return d3.format(',f')(d);
-                    },
-                    axisLabelDistance: 12
-                },
-                y2Axis: {
-                    axisLabel: 'Y2 Axis',
-                    tickFormat: function(d) {
-                        return '$' + d3.format(',.2f')(d)
-                    }
-                },
-                y3Axis: {
-                    tickFormat: function(d){
-                        return d3.format(',f')(d);
-                    }
-                },
-                y4Axis: {
-                    tickFormat: function(d) {
-                        return '$' + d3.format(',.2f')(d)
-                    }
-                }
-            }
+        // Todo: Use this to sort by performance (Malisa)
+        vm.SortByKey = function(array, key) {
+            return array.sort(function(a, b) {
+                var x = a[key]; var y = b[key];
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            });
         };
 
-        vm.data = [
-            {
-                "key" : "Quantity" ,
-                "bar": true,
-                "values" : [ [ 1136005200000 , 1271000.0] , [ 1138683600000 , 1271000.0] , [ 1141102800000 , 1271000.0] , [ 1143781200000 , 0] , [ 1146369600000 , 0] , [ 1149048000000 , 0] , [ 1151640000000 , 0] , [ 1154318400000 , 0] , [ 1156996800000 , 0] , [ 1159588800000 , 3899486.0] , [ 1162270800000 , 3899486.0] , [ 1164862800000 , 3899486.0] , [ 1167541200000 , 3564700.0] , [ 1170219600000 , 3564700.0] , [ 1172638800000 , 3564700.0] , [ 1175313600000 , 2648493.0] , [ 1177905600000 , 2648493.0] , [ 1180584000000 , 2648493.0] , [ 1183176000000 , 2522993.0] , [ 1185854400000 , 2522993.0] , [ 1188532800000 , 2522993.0] , [ 1191124800000 , 2906501.0] , [ 1193803200000 , 2906501.0] , [ 1196398800000 , 2906501.0] , [ 1199077200000 , 2206761.0] , [ 1201755600000 , 2206761.0] , [ 1204261200000 , 2206761.0] , [ 1206936000000 , 2287726.0] , [ 1209528000000 , 2287726.0] , [ 1212206400000 , 2287726.0] , [ 1214798400000 , 2732646.0] , [ 1217476800000 , 2732646.0] , [ 1220155200000 , 2732646.0] , [ 1222747200000 , 2599196.0] , [ 1225425600000 , 2599196.0] , [ 1228021200000 , 2599196.0] , [ 1230699600000 , 1924387.0] , [ 1233378000000 , 1924387.0] , [ 1235797200000 , 1924387.0] , [ 1238472000000 , 1756311.0] , [ 1241064000000 , 1756311.0] , [ 1243742400000 , 1756311.0] , [ 1246334400000 , 1743470.0] , [ 1249012800000 , 1743470.0] , [ 1251691200000 , 1743470.0] , [ 1254283200000 , 1519010.0] , [ 1256961600000 , 1519010.0] , [ 1259557200000 , 1519010.0] , [ 1262235600000 , 1591444.0] , [ 1264914000000 , 1591444.0] , [ 1267333200000 , 1591444.0] , [ 1270008000000 , 1543784.0] , [ 1272600000000 , 1543784.0] , [ 1275278400000 , 1543784.0] , [ 1277870400000 , 1309915.0] , [ 1280548800000 , 1309915.0] , [ 1283227200000 , 1309915.0] , [ 1285819200000 , 1331875.0] , [ 1288497600000 , 1331875.0] , [ 1291093200000 , 1331875.0] , [ 1293771600000 , 1331875.0] , [ 1296450000000 , 1154695.0] , [ 1298869200000 , 1154695.0] , [ 1301544000000 , 1194025.0] , [ 1304136000000 , 1194025.0] , [ 1306814400000 , 1194025.0] , [ 1309406400000 , 1194025.0] , [ 1312084800000 , 1194025.0] , [ 1314763200000 , 1244525.0] , [ 1317355200000 , 475000.0] , [ 1320033600000 , 475000.0] , [ 1322629200000 , 475000.0] , [ 1325307600000 , 690033.0] , [ 1327986000000 , 690033.0] , [ 1330491600000 , 690033.0] , [ 1333166400000 , 514733.0] , [ 1335758400000 , 514733.0]]
-            },
-            {
-                "key" : "Price" ,
-                "values" : [ [ 1136005200000 , 71.89] , [ 1138683600000 , 75.51] , [ 1141102800000 , 68.49] , [ 1143781200000 , 62.72] , [ 1146369600000 , 70.39] , [ 1149048000000 , 59.77] , [ 1151640000000 , 57.27] , [ 1154318400000 , 67.96] , [ 1156996800000 , 67.85] , [ 1159588800000 , 76.98] , [ 1162270800000 , 81.08] , [ 1164862800000 , 91.66] , [ 1167541200000 , 84.84] , [ 1170219600000 , 85.73] , [ 1172638800000 , 84.61] , [ 1175313600000 , 92.91] , [ 1177905600000 , 99.8] , [ 1180584000000 , 121.191] , [ 1183176000000 , 122.04] , [ 1185854400000 , 131.76] , [ 1188532800000 , 138.48] , [ 1191124800000 , 153.47] , [ 1193803200000 , 189.95] , [ 1196398800000 , 182.22] , [ 1199077200000 , 198.08] , [ 1201755600000 , 135.36] , [ 1204261200000 , 125.02] , [ 1206936000000 , 143.5] , [ 1209528000000 , 173.95] , [ 1212206400000 , 188.75] , [ 1214798400000 , 167.44] , [ 1217476800000 , 158.95] , [ 1220155200000 , 169.53] , [ 1222747200000 , 113.66] , [ 1225425600000 , 107.59] , [ 1228021200000 , 92.67] , [ 1230699600000 , 85.35] , [ 1233378000000 , 90.13] , [ 1235797200000 , 89.31] , [ 1238472000000 , 105.12] , [ 1241064000000 , 125.83] , [ 1243742400000 , 135.81] , [ 1246334400000 , 142.43] , [ 1249012800000 , 163.39] , [ 1251691200000 , 168.21] , [ 1254283200000 , 185.35] , [ 1256961600000 , 188.5] , [ 1259557200000 , 199.91] , [ 1262235600000 , 210.732] , [ 1264914000000 , 192.063] , [ 1267333200000 , 204.62] , [ 1270008000000 , 235.0] , [ 1272600000000 , 261.09] , [ 1275278400000 , 256.88] , [ 1277870400000 , 251.53] , [ 1280548800000 , 257.25] , [ 1283227200000 , 243.1] , [ 1285819200000 , 283.75] , [ 1288497600000 , 300.98] , [ 1291093200000 , 311.15] , [ 1293771600000 , 322.56] , [ 1296450000000 , 339.32] , [ 1298869200000 , 353.21] , [ 1301544000000 , 348.5075] , [ 1304136000000 , 350.13] , [ 1306814400000 , 347.83] , [ 1309406400000 , 335.67] , [ 1312084800000 , 390.48] , [ 1314763200000 , 384.83] , [ 1317355200000 , 381.32] , [ 1320033600000 , 404.78] , [ 1322629200000 , 382.2] , [ 1325307600000 , 405.0] , [ 1327986000000 , 456.48] , [ 1330491600000 , 542.44] , [ 1333166400000 , 599.55] , [ 1335758400000 , 583.98]]
-            }
-        ].map(function(series) {
-                series.values = series.values.map(function(d) { return {x: d[0], y: d[1] } });
-                return series;
+        vm.getStockByDistrict = function(startMonth, endMonth, district, vaccine) {
+
+            vm.startMonth ? vm.startMonth : "Nov 2015";
+            vm.endMonth = vm.endMonth ? vm.endMonth : "Dec 2016";
+            //Todo: Temporarily disable filtering by district for the table
+            district = ""
+            vm.district = "";
+            vm.vaccine = vm.selectedVaccine ? vm.selectedVaccine.name : "";
+
+            StockService.getStockByDistrict(startMonth, endMonth, district, vaccine)
+                .then(function(data) {
+
+                vm.data = angular.copy(data);
+                vm.tableParams = new NgTableParams({
+                    page: 1,
+                    count: 15
+                }, {
+                    filterDelay: 0,
+                    counts: [],
+                    data: vm.data,
+                });
+
+                // calculate totals
+                var total = 0;
+                for(var i = 0; i < vm.data.length; i++){
+                    var units = vm.data[i].at_hand;
+                    total += units;
+                }
+
+                shellScope.child.stockathand = total;
+
+                // construct graph data
+                var graphdata = [];
+                var series = [];
+                var min_series = [];
+                var max_series = [];
+
+                for (var i = 0; i < vm.data.length ; i++) {
+                    series.push([vm.data[i].district_name, vm.data[i].received])
+                    min_series.push([vm.data[i].district_name, vm.data[i].stock_requirement__minimum])
+                    max_series.push([vm.data[i].district_name, vm.data[i].stock_requirement__maximum])
+                }
+                graphdata.push({
+                        key: "Min",
+                        values: min_series
+                });
+                graphdata.push({
+                        key: "Issued",
+                        values: series
+                });
+
+                graphdata.push({
+                        key: "Max",
+                        values: max_series
+                });
+
+                vm.graph = graphdata;
+
+                // update graph
+                vm.options = {
+                        chart: {
+                            type: 'multiBarChart',
+                            height: 500,
+
+                            title: {
+                                enable: true,
+                                text: 'VACCINE DISTRIBUTION'
+                            },
+                            showLegend: true,
+                            stacked: true,
+                            showControls: true,
+                            margin : {
+                                top: 20,
+                                right: 20,
+                                bottom: 85,
+                                left: 65
+                            },
+                            groupSpacing: 0.2,
+                            rotateLabels: 45,
+                            staggerLabels: true,
+                            x: function(d){ return d[0]; },
+                            y: function(d){ return d[1]; },
+                            showValues: true,
+                            valueFormat: function(d){
+                                return tickFormat(d3.format(',.1f'));
+                            },
+                            transitionDuration: 500,
+                        }
+                };
             });
-})
+        };
+
+        $scope.$on('refresh', function(e, startMonth, endMonth, district, vaccine) {
+            if(startMonth.name && endMonth.name && district.name && vaccine.name)
+            {
+
+                vm.getStockByDistrict(startMonth.name, endMonth.name, district.name, vaccine.name);
+                vm.getStockByMonth(startMonth.name, endMonth.name, district.name, vaccine.name);
+            }
+        });
+
+    }
+
+]);
