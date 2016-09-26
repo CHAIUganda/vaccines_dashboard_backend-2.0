c493d0f11c3100252665125d8e23c0fdbe08a9eb
BCHAI <bjuubsm@gmail.com>
2016-09-26 11:55:34 +0300

    set PENTA as default Vaccine

--- dashboard/static/app/shared/mainController.js
+++ dashboard/static/app/shared/mainController.js
@@ -31,7 +31,7 @@ angular.module('dashboard')
 
         FilterService.getVaccines().then(function(data) {
             shell.vaccines = data;
-            shell.selectedVaccine = shell.vaccines[6];
+            shell.selectedVaccine = shell.vaccines[5];
         });
 
 
