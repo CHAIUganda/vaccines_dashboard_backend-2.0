ad1cf6eb98921dea46798b037a6e927d9e5db1d0
smagero <magerosteve@gmail.com>
2016-10-20 08:15:21 +0300

    cold chain filters

--- cold_chain/urls.py
+++ cold_chain/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url, include
 
 from . import views
 
+import cold_chain.views.api
+import dashboard.views.main
+
+
 urlpatterns = [
+    url(r'^$', dashboard.views.main.FridgeView.as_view(), name='cold'),
+
+    url(r'^api/districts', cold_chain.views.api.Districts.as_view(), name='district'),
+    url(r'^api/carelevels', cold_chain.views.api.CareLevels.as_view(), name='carelevel'),
+    url(r'^api/quarters', cold_chain.views.api.Quarters.as_view(), name='quarter'),
+
 ]
