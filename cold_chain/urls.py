da44c1a0e9b6db0d5a473d3402af3bdc8ab4b807
BCHAI <bjuubsm@gmail.com>
2016-10-20 15:23:33 +0300

    coldchain api

--- cold_chain/urls.py
+++ cold_chain/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^api/districts', cold_chain.views.api.Districts.as_view(), name='district'),
     url(r'^api/carelevels', cold_chain.views.api.CareLevels.as_view(), name='carelevel'),
     url(r'^api/quarters', cold_chain.views.api.Quarters.as_view(), name='quarter'),
+    url(r'^api/immunizingfacilities', cold_chain.views.api.ImmunizingFacilities.as_view(), name='immunizingfacilities'),
 
 ]
--- cold_chain/views/api.py
+++ cold_chain/views/api.py
@@ -31,3 +31,25 @@ class CareLevels(APIView):
     def get(self, request):
         carelevels = FacilityType.objects.all().values('group').distinct().order_by('group')
         return Response(carelevels)
+
+
+class ImmunizingFacilities(APIView):
+    def get(self, request):
+        district = request.query_params.get('district', None)
+        carelevel = request.query_params.get('group', None)
+        quarter = request.query_params.get('quarter', None)
+
+        summary = ImmunizingFacility.objects.filter()\
+                .order_by('facility__facility__code' )\
+                .values(
+                        'facility__facility__district',
+                        'facility__facility_type_id',
+                        'facility__facility__name',
+                        'facility',
+                        'facility__group',
+                        'facility__facility__code',
+                        'quarter',
+                        'static',
+                        'outreach',
+                        'ficc_storage')
+        return Response(summary)
