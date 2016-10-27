7af0d27c3e8dea2f333f805bb0a1de7f723ee39d
Brian Wamala <bjuubsm@gmail.com>
2016-10-27 12:38:39 +0300

    Facilities Tabledata
    facilities.html
    fridgecontroller.js

--- dashboard/static/app/components/fridge/fridgeController.js
+++ dashboard/static/app/components/fridge/fridgeController.js
@@ -267,14 +267,11 @@ angular.module('dashboard')
             });
         };
 
-
-
-
-
         vm.getFridgeDistrictImmunizingFacility = function(startQuarter, endQuarter, district, carelevel) {
 
             vm.startQuarter ? vm.startQuarter : "201601";
             vm.endQuarter = vm.endQuarter ? vm.endQuarter : "201604";
+            district = "";
             vm.district = district;
             vm.carelevel = carelevel;
 
@@ -282,6 +279,19 @@ angular.module('dashboard')
                 .then(function(data) {
 
                 vm.data = angular.copy(data);
+                tabledataAllfridge = vm.data.filter(
+                        function (value) {
+                            return value;
+                        });
+                vm.tableParamsAllfridge = new NgTableParams({
+                    page: 1,
+                    count: 10
+                }, {
+                    filterDelay: 0,
+                    counts: [],
+                    data: tabledataAllfridge,
+                    });
+
                 vm.tableParams_d = new NgTableParams({
                     page: 1,
                     count: 15
@@ -296,97 +306,67 @@ angular.module('dashboard')
                 shellScope.child.carelevel = vm.carelevel;
 
 
-                // construct Distribution graph data
-                var graphdataDistribution = [];
-                var seriesDistribution = [];
-                var seriesOrders = [];
-                var min_seriesDistribution = [];
-                var max_seriesDistribution = [];
-                shellScope.child.refreshrate = 0;
+                // construct District graph data
+                var graphdata = [];
+                var seriesRequired = [];
+                var seriesAvailable = [];
+                var seriesGap = [];
 
                 for (var i = 0; i < vm.data.length ; i++) {
-                    seriesDistribution.push([vm.data[i].month, parseInt(vm.data[i].received)])
-                    seriesOrders.push([vm.data[i].month, vm.data[i].ordered])
-                    min_seriesDistribution.push([vm.data[i].month, vm.data[i].stock_requirement__minimum])
-                    max_seriesDistribution.push([vm.data[i].month, vm.data[i].stock_requirement__maximum])
-                    if (vm.data[i].month == MonthService.getMonthNumber(endMonth.split(" ")[0])){
-                        shellScope.child.refreshrate = vm.data[i].ordered == 0 ? 0 :vm.data[i].received/vm.data[i].ordered*100 ;
-                    }
+                    seriesRequired.push([vm.data[i].quarter, vm.data[i].required])
+                    seriesAvailable.push([vm.data[i].quarter, vm.data[i].available])
+                    seriesGap.push([vm.data[i].quarter, vm.data[i].gap])
+
                 }
-                graphdataDistribution.push({
-                        key: "Min",
-                        values: min_seriesDistribution,
+/*
+                seriesRequired = [[201602, 30], [201603, 30]];
+                seriesAvailable = [[201602, 60], [201603, 20]];
+*/
+
+                graphdata.push({
+                        key: "Required",
+                        values: seriesRequired,
                         color:'#A5E816'
                 });
-                graphdataDistribution.push({
-                        key: "Issued",
-                        values: seriesDistribution,
+                graphdata.push({
+                        key: "Available",
+                        values: seriesAvailable,
                         color:'#1F77B4'
                 });
-                graphdataDistribution.push({
-                        key: "Ordered",
-                        values: seriesOrders,
+/*                graphdata.push({
+                        key: "Gap",
+                        values: seriesGap,
                         color:'red'
                 });
-
-                graphdataDistribution.push({
-                        key: "Max",
-                        values: max_seriesDistribution,
-                        color:'#FF7F0E'
-                });
-
-                vm.graphDistribution = graphdataDistribution;
+*/
+                vm.graph = graphdata;
 
 
-                // update Distribution graph
-                vm.optionsDistribution = {
+                // update graph
+                vm.options = {
                         chart: {
-                            type: 'lineChart',
-                            height: 500,
-                            width : 500,
-                            title: {
-                                enable: true,
-                                text: 'Abim'
+                            type: "multiBarChart",
+                            height: 450,
+                            margin: {
+                              top: 20,
+                              right: 20,
+                              bottom: 45,
+                              left: 45
                             },
-                            showLegend: true,
-                            stacked: true,
-                            showControls: true,
-                            margin : {
-                                top: 20,
-                                right: 20,
-                                bottom: 85,
-                                left: 65
-                            },
-                            forceY: ([0,100]),
-                            staggerLabels: true,
+                            clipEdge: true,
+                            stacked: false,
                             x: function(d){ return d[0]; },
                             y: function(d){ return d[1]; },
-                            xAxis: {
-                                axisLabel: 'Months',
-                                tickFormat: function(d){
-                                                return MonthService.getMonthName(d);
-                                            },
-                                axisLabelDistance: 10
-                            },
-                            useInteractiveGuideline: true,
-                            dispatch: {
-                            stateChange: function(e){ console.log("stateChange"); },
-                            changeState: function(e){ console.log("changeState"); },
-                            tooltipShow: function(e){ console.log("tooltipShow"); },
-                            tooltipHide: function(e){ console.log("tooltipHide"); }
-                            },
                             showValues: true,
-                            valueFormat: function(d){
-                                return tickFormat(d3.format(',.1f'));
-                            },
-                            transitionDuration: 500,
-                        }
+                            //valueFormat: function(d){
+                            //    return tickFormat(d3.format(',.1f'));
+                            //}
+                        },
                 };
 
 
             });
         };
-
 
         $scope.$on('refreshCapacity', function(e, startQuarter, endQuarter, district, carelevel) {
             if(startQuarter && endQuarter)
