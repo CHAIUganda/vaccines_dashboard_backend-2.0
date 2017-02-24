2b5a77fcbce7d7080da308dcee60b7965f535281
Brian Wamala <bjuubsm@gmail.com>
2017-02-24 17:01:36 +0300

    changes on default endMonth

--- dashboard/static/app/shared/mainController.js
+++ dashboard/static/app/shared/mainController.js
@@ -27,7 +27,7 @@ angular.module('dashboard')
         FilterService.getLastPeriod().then(function(data) {
             shell.defaultPeriod = data;
             shell.defaultMonth = parseInt(data.period.toString().substring(4, 6));
-            shell.endMonth = shell.months[shell.defaultMonth-1];
+            shell.endMonth = shell.months[shell.defaultMonth-2];
         });
 
         shell.stockathand = 0;
