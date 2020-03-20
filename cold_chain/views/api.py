fdd38c4b6b65b84dfc1ab164925587c6fe5ea2a0
Kigenyi Phillip <codephillip@gmail.com>
2020-03-20 11:38:08 +0300

    refactored coldchain code, removed unused code

--- cold_chain/views/api.py
+++ cold_chain/views/api.py
@@ -31,309 +31,10 @@ class Districts(APIView):
         return Response(districts)
 
 
-class FacilityTypes(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        kwargs = {}
-        if district and district.lower() != 'national':
-            kwargs.update({'district': district})
-        facility_types = ColdChainFacility.objects.filter(**kwargs).values('type__group').annotate(count=Count('id'))
-        return Response(facility_types)
-
-
 class CareLevels(APIView):
     def get(self, request):
-        carelevels = FacilityType.objects.all().values('group').distinct().order_by('group')
-        return Response(carelevels)
-
-
-class ImmunizingFacilities(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = ImmunizingFacility.objects.filter(**args) \
-            .values(
-            'quarter',
-            'static',
-            'outreach',
-            'ficc_storage',
-            'facility',
-            'facility__district',
-            'facility__name',
-            'facility__type__group',
-            'facility__code')
-        return Response(summary)
-
-
-class DistrictImmunizingFacilities(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = ImmunizingFacility.objects.filter(**args) \
-            .values('facility__district') \
-            .annotate(immunizing=(Count(Q(static='True') | Q(outreach='True'))),
-                      immunizing_with_fridge=(Count(Q(static='True') & Q(outreach='True') & Q(ficc_storage='False'))),
-                      Total_facilities=(Count('facility__code'))) \
-            .order_by('facility__district') \
-            .values(
-            'facility__district',
-            'immunizing',
-            'Total_facilities',
-            'quarter',
-            'immunizing_with_fridge'
-        )
-        return Response(summary)
-
-
-class Refrigerators(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Refrigerator.objects.filter() \
-            .values(
-            'number_existing',
-            'working_well',
-            'needs_maintenance',
-            'not_working',
-            'facility__district',
-            'facility__name',
-            'quarter')
-        return Response(summary)
-
-
-class DistrictRefrigerators(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201602')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district and district.lower() != 'national':
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Refrigerator.objects.filter(**args) \
-            .values('facility__district') \
-            .annotate(working_well=Sum('working_well'),
-                      needs_maintenance=Sum('needs_maintenance'),
-                      not_working=Sum('not_working'),
-                      total_facilities=Count('facility__code'),
-                      number_existing=Sum('number_existing')) \
-            .order_by('-not_working') \
-            .values(
-            'working_well',
-            'needs_maintenance',
-            'not_working',
-            'quarter',
-            'number_existing',
-            'total_facilities',
-            'facility__district')
-        return Response(summary)
-
-
-class FacilityRefrigerators(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', 'all')
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Refrigerator.objects.filter(**args) \
-            .values('facility__name') \
-            .annotate(working_well=Sum('working_well'),
-                      needs_maintenance=Sum('needs_maintenance'),
-                      not_working=Sum('not_working'),
-                      number_existing=Sum('number_existing')) \
-            .order_by('-not_working') \
-            .values(
-            'working_well',
-            'needs_maintenance',
-            'not_working',
-            'quarter',
-            'number_existing',
-            'facility__name')
-        return Response(summary)
-
-
-class Capacities(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Capacity.objects.filter(**args) \
-            .order_by('facility__district') \
-            .values(
-            'actual',
-            'required',
-            'difference',
-            'quarter',
-            'facility',
-            'facility__district',
-            'facility__name',
-            'facility__type__group')
-        return Response(summary)
-
-
-class DistrictCapacities(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district:
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Capacity.objects.filter(**args) \
-            .values('facility__district') \
-            .annotate(required=Sum(F('required')),
-                      available=Sum(F('actual')),
-                      difference=Sum(F('required') - F('actual')),
-
-                      surplus=Case(
-                          When(Q(difference=Value(0)) | Q(required=Value(0)), then=Value(0)),
-                          default=(
-                              ExpressionWrapper(100 * F('difference') / F('required'), output_field=IntegerField()))
-                      )
-                      ) \
-            .order_by('difference') \
-            .values(
-            'available',
-            'required',
-            'difference',
-            'surplus',
-            'quarter',
-            'facility__district')
-        return Response(summary)
-
-
-class FacilityCapacities(APIView):
-    def get(self, request):
-        district = request.query_params.get('district', None)
-        carelevel = request.query_params.get('carelevel', None)
-        startQuarter = request.query_params.get('startQuarter', '201601')
-        endQuarter = request.query_params.get('endQuarter', None)
-
-        # Create arguments for filtering
-        args = {'quarter__gte': startQuarter}
-
-        if district and district.lower() != "national":
-            args.update({'facility__district': district})
-
-        if carelevel:
-            args.update({'facility__type__group': carelevel})
-
-        if endQuarter:
-            args.update({'quarter__lte': endQuarter})
-
-        summary = Capacity.objects.filter(**args) \
-            .values('facility__name') \
-            .annotate(required=F('required'),
-                      actual=F('actual'),
-                      difference=(F('required') - F('actual')),
-                      adequate=(F('actual') - F('required')),
-                      surplus=Case(
-                          When(Q(adequate=Value(0)) | Q(required=Value(0)), then=Value(0)),
-                          default=(ExpressionWrapper(100 * F('adequate') / F('required'), output_field=IntegerField()))
-                      )) \
-            .order_by('-difference') \
-            .values(
-            'actual',
-            'adequate',
-            'required',
-            'difference',
-            'surplus',
-            'quarter',
-            'facility__name')
-        return Response(summary)
+        facility_types = FacilityType.objects.all().values('name')
+        return Response(facility_types)
 
 
 class RequestSuperClass(APIView):
@@ -428,9 +129,9 @@ class FunctionalityMetricsGraph(RequestSuperClass):
                 not_working = not_working_per_half.count()
                 needs_repair = needs_repair_per_half.count()
 
-                percentages_object = self.generate_percentages(needs_repair, not_working, working, self.start_year,
-                                                               quarter)
-                statistics.append(percentages_object)
+                functionality_percentages = self.generate_functionality_percentages(needs_repair, not_working, working, self.start_year,
+                                                                             quarter)
+                statistics.append(functionality_percentages)
             self.start_year = self.start_year + 1
         try:
             functionality_percentage = round((functionality_working_total /
@@ -442,7 +143,7 @@ class FunctionalityMetricsGraph(RequestSuperClass):
             print(e)
         return Response(statistics)
 
-    def generate_percentages(self, needs_repair, not_working, working, year, quarter):
+    def generate_functionality_percentages(self, needs_repair, not_working, working, year, quarter):
         working_percentage = 0
         not_working_percentage = 0
         needs_repair_percentage = 0
@@ -777,8 +478,8 @@ class OptimalityStats(RequestSuperClass):
         optimal = Sum(Case(When(refrigerator__supply_year__lte=ten_years_ago_date, then=0),
                            When(refrigerator__supply_year__gte=ten_years_ago_date, then=1),
                            output_field=IntegerField()))
-        facilitys = ColdChainFacility.objects.annotate(optimal=optimal).filter(type__name=facility_type).select_related(
-            'district')
+        facilitys = ColdChainFacility.objects.annotate(optimal=optimal).filter(type__name=facility_type) \
+            .select_related('district')
         optimal_facilitys = facilitys.filter(optimal__gt=0)
 
         districts = [facility.district for facility in facilitys]
